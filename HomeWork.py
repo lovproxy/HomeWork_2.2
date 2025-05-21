f37edb7b4a7bed262bc0e535422baa4f8b05c739
Fedor <rva.pva.85@bk.ru>
2025-05-21 11:32:40 +0300

    Init

---
+++ HomeWork.py
@@ -0,0 +1,21 @@
+class Solver():
+    def __init__(self):
+        pass
+
+    def solver(self, board, priority):
+        pass
+
+    def isSolvable(self):
+        return ''
+
+    def moves(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def solve(self):
+        pass
