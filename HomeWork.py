5cf68585a4d99298a73ce1dcbe0109718663fe0e
qpei007 <mihail.7658@gmail.com>
2025-05-23 23:16:30 +0300

    Добавлены комментарии

--- HomeWork.py
+++ HomeWork.py
@@ -176,20 +176,20 @@ class Solver():
 class Board:
     def __init__(self, blocks):  # создает поле из массива блоков NxN blocks (block[i][j] =
         # номер блока в i-той строке и j-том столбце)
-        if isinstance(blocks, list):
-            self.N = int(math.sqrt(len(blocks)))
-            self.board = np.ones((self.N, self.N))
+        if isinstance(blocks, list): # проверяем, что мы получили на вход матрицу или список и создаём доску
+            self.N = int(math.sqrt(len(blocks))) # определяем размерность доски
+            self.board = np.ones((self.N, self.N)) # создаём матрицу
             self.board = self.board.astype(int)
-            for i in range(self.N):
+            for i in range(self.N): # заполняем матрицу входными данными
                 line = blocks[self.N * i:self.N * (i + 1)]
                 self.board[i] = line
-        if isinstance(blocks, np.ndarray):
+        if isinstance(blocks, np.ndarray): # если на вход получили матрицу, то просто копируем её в доску
             self.board = blocks.copy()
-            self.N = blocks.shape[0]
-
+            self.N = blocks.shape[0] # определяем размерность доски
+        # создаём итоговую доску
         self.target_board = np.arange(1, self.N * self.N + 1).reshape((self.N, self.N))
         self.target_board[-1, -1] = 0
-
+        # определяем позицию пустого места
         i, j = np.where(self.board == 0)
         i = int(i[0])
         j = int(j[0])
@@ -204,6 +204,7 @@ class Board:
 
     def hamming(self): # возвращает количество отличий от целевой доски
         mistakes = 0
+        # для всех строк для всех элементов кроме последнего элемента последней строки сравниваем правильный элемент с элементов, который стоит в нашей доске и если он неправильный увеличиваем счетчик
         for i in range(self.N - 1):
             for j in range(self.N):
                 if self.target_board[i][j] != self.board[i][j]:
@@ -215,15 +216,16 @@ class Board:
 
     def manhattan(self):
         length = 0
+        # для всех строк для всех элементов кроме последнего элемента последней строки находим позицию элемента в нашей доске и ищем как далеко он олт правильного места
         for i in range(self.N - 1):
             for j in range(self.N):
                 elem = self.target_board[i][j]
-                col, row = np.where(self.board == elem)
-                length += abs(i - col[0]) + abs(j - row[0])
+                col, row = np.where(self.board == elem) # находим позицию элемента
+                length += abs(i - col[0]) + abs(j - row[0]) # добавляем в вывод расстояние до правильной позиции
         for j in range(self.N - 1):
             elem = self.target_board[self.N - 1][j]
-            col, row = np.where(self.board == elem)
-            length += abs(self.N - 1 - col[0]) + abs(j - row[0])
+            col, row = np.where(self.board == elem) # находим позицию элемента
+            length += abs(self.N - 1 - col[0]) + abs(j - row[0]) # добавляем в вывод расстояние до правильной позиции
 
         return length
 
@@ -233,11 +235,12 @@ class Board:
         return np.array_equal(self.board, self.target_board)
 
     def twin(self): # меняет два соседних блока в строке и возвращает копию доски
-        twin_board = self.board.copy()
+        twin_board = self.board.copy() # копируем доску в новую переменную
+        # ищем два непустых соседних элемента в строке
         for i in range(self.N):
             for j in range(self.N - 1):
                 if twin_board[i, j] != 0 and twin_board[i, j + 1] != 0:
-                    twin_board[i, j], twin_board[i, j + 1] = twin_board[i, j + 1], twin_board[i, j]
+                    twin_board[i, j], twin_board[i, j + 1] = twin_board[i, j + 1], twin_board[i, j] # меняем местами
                     return Board(twin_board)
 
     def __eq__(self, board): # сравнивает две доски с помощью оператора сравнения
@@ -246,21 +249,22 @@ class Board:
 
     def __iter__(self):
         self._neighbors = []
-        i, j = self.blank_pos
-        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        i, j = self.blank_pos # позиция пустого места
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)] # все возможные ходы
         for i_move, j_move in moves:
+            # ищем новую позицию элемента
             new_i = i + i_move
             new_j = j + j_move
-            if 0 <= new_i < self.N and 0 <= new_j < self.N:
-                new_board = self.board.copy()
-                new_board[i, j], new_board[new_i, new_j] = new_board[new_i, new_j], new_board[i, j]
-                self._neighbors.append(Board(new_board))
+            if 0 <= new_i < self.N and 0 <= new_j < self.N: # проверяем можем ли так походить
+                new_board = self.board.copy() # создаем новую доску
+                new_board[i, j], new_board[new_i, new_j] = new_board[new_i, new_j], new_board[i, j] # меняем местами пустое место и элемент
+                self._neighbors.append(Board(new_board)) # добавляем новую доску в список соседей
 
         self._current = 0
         return self
 
 
-    def __next__(self):
+    def __next__(self): # выводим соседей пока они есть
         if self._current >= len(self._neighbors):
             raise StopIteration
         result = self._neighbors[self._current]
